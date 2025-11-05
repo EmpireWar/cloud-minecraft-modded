28042be8f8f6334e4aebc3bb46f8c2e7577f3cb8
SamB440 <sam@islandearth.net>
2025-11-05 17:14:51 +0000

    fix

--- cloud-sponge/build.gradle.kts
+++ cloud-sponge/build.gradle.kts
@@ -3,18 +3,20 @@ import org.spongepowered.gradle.vanilla.repository.MinecraftPlatform
 plugins {
     id("conventions.base")
     id("conventions.publishing")
-    id("org.spongepowered.gradle.vanilla")
+    id("net.neoforged.moddev")
 }
 
 dependencies {
     api(libs.cloud.core)
     implementation(libs.cloud.brigadier)
+    offlineLinkedJavadoc(project(":cloud-minecraft-modded-common"))
     implementation(project(":cloud-minecraft-modded-common"))
     compileOnly("org.spongepowered:spongeapi:14.0.0-SNAPSHOT")
     compileOnly("org.spongepowered:sponge:1.21.4-14.0.0-SNAPSHOT")
 }
 
-minecraft {
-    version("1.21.4")
-    platform(MinecraftPlatform.JOINED)
+neoForge {
+    enable {
+        neoFormVersion = "1.21.9-20250930.151910"
+    }
 }
